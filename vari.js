052561e095e865a5c72c61f76c34e22976969e99
Raman kumar <ramankumarlal@outlook.com>
2023-01-19 04:08:47 +0000

    rating

--- vari.js
+++ vari.js
@@ -1,5 +1,19 @@
-let value1 = "23";
-let value2 = 25;
-let result = value1 +value2
+// let value1 = "23";
+// let value2 = 25;
+// let result = value1 +value2
 
-console.log(result)
+// console.log(result)
+
+let ab = 8;
+let abc = ab+1;
+
+// console.log(abc);
+
+let rating = 9;
+ 
+if (rating ==5) 
+{
+    console.log("Excellent");
+} else {
+    console.log("Below 5")
+}
